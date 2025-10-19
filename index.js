74ca4798f699d40a196ae2c2295028a7217e0f41
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-20 00:15:59 +0200

    code refactoring

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ const url = require('url');
 
 // SERVER
 
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+    
+
 const server = http.createServer((req, res) => {
 
     const pathName = req.url;
@@ -42,12 +47,10 @@ const server = http.createServer((req, res) => {
     } else if ( pathName === '/product'){
         res.end('This is product.')
     } else if ( pathName === '/api'){
-        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
-            const productData = JSON.parse(data);
-            res.writeHead(200, { 'Content-type': 'application/json'})
-            res.end(data);
-        });
-         
+        
+      res.writeHead(200, { 'Content-type': 'application/json'})
+      res.end(data);
+
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html'
