db465ab79eb998423ee041fa68c859122186c7a8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-24 23:23:12 +0200

    setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-const slugifyT = require('slugify');
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 
