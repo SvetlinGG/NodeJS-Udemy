99fa7a6cf583b5adf1789dcf43e83026f5f58849
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-28 22:54:59 +0100

    event emitter

--- 2-how-node-works/final/events.js
+++ 2-how-node-works/final/events.js
@@ -1,1 +1,28 @@
+const EventEmitter = require('events');
+const http = require('http');
+
+
+
+class Sales extends EventEmitter {
+  constructor(){
+    super();
+  }
+}
+
+const myEmitter = new Sales();
+
+myEmitter.on('newSale', ()=> {
+  console.log('There was a new sale!');
+});
+
+myEmitter.on('newSale', () => {
+  console.log('Costumer name: Svetlin');
+});
+
+myEmitter.on('newSale', stock => {
+  console.log(`There are now ${stock} items left in stock.`);
+  
+})
+
+myEmitter.emit('newSale');
 
