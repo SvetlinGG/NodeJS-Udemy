8cdad340e3c4584bf35dc2fa26722b4cc0052c8a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-29 22:05:11 +0100

    example for module export - functions

--- 2-how-node-works/final/modules.js
+++ 2-how-node-works/final/modules.js
@@ -1,3 +1,12 @@
 console.log(arguments);
+console.log(require('module').wrapper);
+
+const C = require('./test-module-1');
+const calc1 = new C();
+
+console.log(calc1.add(2, 3));
+console.log(calc1.multiply(2, 3));
+console.log(calc1.divide(2, 3));
+
 
 
