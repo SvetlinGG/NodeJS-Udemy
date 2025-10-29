598c80bc3f33081d5c8fa6794aeaae337f1565cf
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-29 20:59:30 +0100

    start stream

--- 2-how-node-works/final/streams.js
+++ 2-how-node-works/final/streams.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
-  // Solution 1
+  // Solution 1: Using streams (correct approach)
   fs.readFile('test-file.txt', (err, data) => {
     if (err) console.log(err);
     res.end(data)
+  
   });
 });
 
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening...');
-  
+server.listen(8001, '127.0.0.1', () => {
+  console.log('Listening on port 8001...');
 })
