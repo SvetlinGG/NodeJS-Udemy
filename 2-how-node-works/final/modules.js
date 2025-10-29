002eb326d04547568b1b7bb0310a414098e33fa6
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-29 21:57:40 +0100

    modules

--- 2-how-node-works/final/modules.js
+++ 2-how-node-works/final/modules.js
@@ -1,17 +1,3 @@
-// console.log(arguments);
-// console.log(require("module").wrapper);
+console.log(arguments);
 
-// module.exports
-const C = require("./test-module-1");
-const calc1 = new C();
-console.log(calc1.add(2, 5));
 
-// exports
-// const calc2 = require("./test-module-2");
-const { add, multiply } = require("./test-module-2");
-console.log(multiply(2, 5));
-
-// caching
-require("./test-module-3")();
-require("./test-module-3")();
-require("./test-module-3")();
