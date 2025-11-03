a281dd35a1bda076d05c9a5e67b0c3cbdbca2c04
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-03 22:27:31 +0100

    use request Time

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,13 +4,27 @@ const fs = require('fs');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log('Hello from the Middleware!!');
+    next();
+});
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+})
+
+
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req,res) => {
+    console.log(req.requestTime);
+    
     res.status(200).json({
         status: 'success',
+        requestedAt: req.requestTime,
         results: tours.length,
         data: {
             tours
@@ -94,11 +108,6 @@ app
     .route('/api/v1/tours')
     .get(getAllTours)
     .post(createTour);
-
-app.use((req, res, next) => {
-    console.log('Hello from the Middleware!!');
-    next();
-});
 
 app
     .route('/api/v1/tours/:id')
