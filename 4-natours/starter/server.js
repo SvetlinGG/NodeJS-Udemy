f09f0e927acf879afbad4b7d2a4ff5a6473f946c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-06 23:21:10 +0100

    set tha path

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,9 @@
-const app = require('./app')
+const app = require('./app');
+const dotenv = require('dotenv');
 
-console.log(process.env);
+dotenv.config({path: './config.env'});
+//console.log(process.env);
 
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App listening on port: ${port}...`));
