4e0b30d5a8777b30f1fe1d56006c36ae74417b3f
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-11 23:28:23 +0100

    setup mongosh

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 // 3) Routes
 
 app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/tours', userRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4) Start Server
 module.exports = app;
