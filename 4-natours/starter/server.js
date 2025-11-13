203966f906b4d060147f24d84650d63c7f32a720
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-13 22:13:28 +0100

    error message

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,24 +1,18 @@
 const dotenv = require('dotenv');
-
+dotenv.config({ path: './config.env' });
 
 const mongoose = require('mongoose');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace(
   '<DATABASE_PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
 mongoose
-    .connect(DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    })
-    .then(() => console.log('DB connection successful!'));
-    
- 
+    .connect(DB)
+    .then(() => console.log('DB connection successful!'))
+    .catch(err => console.log('DB connection failed:', err.message));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
