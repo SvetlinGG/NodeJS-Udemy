e8b8ab893d9c59ae6c33397429d0ed36e3c7c146
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-13 21:43:46 +0100

    connection with DB

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -28,6 +28,7 @@ exports.updateUser = (req, res) => {
     });
 };
 
+
 exports.deleteUser = (req, res) => {
     res.status(500).json({
         status: 'error',
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,13 +1,21 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
 const mongoose = require('mongoose');
 const app = require('./app');
-const dotenv = require('dotenv');
 
-dotenv.config({path: './config.env'});
+const DB = process.env.DATABASE_LOCAL;
 
-mongoose.connect();
-
+mongoose.connect(DB)
+  .then(() => {
+    console.log('DB connection successful!');
+  })
+  .catch((err) => {
+    console.log('DB connection failed:', err);
+  });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`App listening on port: ${port}...`));
+app.listen(port, () => {
+  console.log(`App listening on port: ${port}...`);
+});
 
