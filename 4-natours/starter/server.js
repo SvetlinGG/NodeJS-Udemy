bffbc7bbedab10e39985417c773b86e105a40fa8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-13 22:10:13 +0100

    setup issue

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,21 +1,26 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+
 
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const DB = process.env.DATABASE_LOCAL;
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<DATABASE_PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-mongoose.connect(DB)
-  .then(() => {
-    console.log('DB connection successful!');
-  })
-  .catch((err) => {
-    console.log('DB connection failed:', err);
-  });
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('DB connection successful!'));
+    
+ 
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App listening on port: ${port}...`);
 });
-
