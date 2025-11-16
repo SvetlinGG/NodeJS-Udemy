4b63dbc68ae8361c51b555a2863156618882dab7
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-16 21:46:24 +0100

    another way of creating documents

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,14 +1,5 @@
 const Tour = require('./../models/tourModels')
 
-exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price){
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Missing name or price'
-        });
-    }
-    next();
-}
 
 
 exports.getAllTours = (req,res) => {
@@ -39,13 +30,26 @@ exports.getTour =  (req,res) => {
     // });
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
+    try {
+         // const newTour = new Tour ({})
+    // newTour.save()
+
+    const newTour = await Tour.create(req.body);
+
     res.status(201).json({
         status: 'success',
         data: {
             tour: newTour
         }
     });
+        
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail'
+        })
+    }
+   
 };
 
 exports.updateTour = (req, res) => {
