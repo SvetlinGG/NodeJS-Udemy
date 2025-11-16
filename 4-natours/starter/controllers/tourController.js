2e40ab36adf15c3a5fe4c6f5957ef335a77c99a2
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-16 22:07:45 +0100

    reading documents

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,17 +2,25 @@ const Tour = require('./../models/tourModels')
 
 
 
-exports.getAllTours = (req,res) => {
-    console.log(req.requestTime);
-    
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        // results: tours.length,
-        // data: {
-        //     tours
-        // }
-    });
+exports.getAllTours = async (req,res) => {
+
+    try {
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: 'success',
+            requestedAt: req.requestTime,
+            results: tours.length,
+            data: {
+                tours
+            }
+        });
+        
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 }
 
 exports.getTour =  (req,res) => {
@@ -46,10 +54,10 @@ exports.createTour = async (req, res) => {
         
     } catch (err) {
         res.status(400).json({
-            status: 'fail'
-        })
+            status: 'fail',
+            message: 'Invalid data sent!'
+        });
     }
-   
 };
 
 exports.updateTour = (req, res) => {
