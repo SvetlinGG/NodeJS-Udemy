60c90d30390de496eebad41302098d045d5a3462
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-16 17:35:49 +0100

    setup models

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,5 +1,4 @@
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
@@ -14,35 +13,9 @@ mongoose
     .then(() => console.log('DB connection successful!'))
     .catch(err => console.log('DB connection failed:', err.message));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
 
-const Tour = mongoose.model('Tour', tourSchema);
 
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 497
-});
 
-testTour.save().then(doc => {
-  console.log(doc);
-}).catch(err => {
-  console.log('ERROR: ', err);
-});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
