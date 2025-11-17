a960a6b8b9d8a035308faca0db267dd9d2eec791
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-17 22:48:06 +0100

    more changes of modelling tours

--- 4-natours/starter/models/tourModels.js
+++ 4-natours/starter/models/tourModels.js
@@ -40,7 +40,17 @@ const tourSchema = new mongoose.Schema({
     description: {
       type: String,
       trim: true
-    }
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image']
+    },
+    image: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    startDates: [Date]
   });
   
 
