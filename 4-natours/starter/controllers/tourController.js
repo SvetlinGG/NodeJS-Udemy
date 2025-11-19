d6302dc4e56143704086152b065d73936489af19
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-19 21:37:51 +0100

    more filtering

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -5,21 +5,23 @@ const Tour = require('./../models/tourModels')
 exports.getAllTours = async (req,res) => {
 
     try {
+        // BUILD QUERY
         const queryObj = {...req.query};
         const excludeFields = ['page', 'sort', 'limit', 'fields'];
         excludeFields.forEach( el => delete queryObj[el]);
-
-        console.log(req.query, queryObj);
         
-        const tours = await Tour.find(req.query);
+        const tours = await Tour.find(queryObj);
 
-        // const tour = await Tour.find()
+        // const query = Tour.find()
         // .where('duration')
         // .equals(5)
         // .where('difficulty')
         // .equals('easy');
 
+        // EXECUTE QUERY
+        const tours = await query;
 
+        // SEND RESPONSE
         res.status(200).json({
             status: 'success',
             requestedAt: req.requestTime,
