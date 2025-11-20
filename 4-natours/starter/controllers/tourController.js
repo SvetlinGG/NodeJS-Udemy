0d65dd22e3d3e8c6f9c829228b87388937406624
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-20 20:11:50 +0100

    pagination

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -36,6 +36,15 @@ exports.getAllTours = async (req,res) => {
             query = query.select('-__v')
         }
 
+
+        // 4) Pagination
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+        // page=2&limit=10 , 1-10, page 1, 11-20, page 2, 21-30 page 3
+        query = query.skip(skip).limit(limit)
+
+
         // EXECUTE QUERY
         const tours = await query;
 
