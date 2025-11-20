0e1783955d3319b977aceda8d6ddfe7fe5ca1fe2
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-20 21:14:45 +0100

    refactoring the pagination code

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -19,7 +19,7 @@ class APIFeatures {
         let queryStr = JSON.stringify(queryObj)
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
         
-        this.query.find(JSON.parse(queryStr));
+        this.query = this.query.find(JSON.parse(queryStr));
        // let query = await Tour.find(JSON.parse(queryStr));
        return this;
     }
@@ -45,16 +45,12 @@ class APIFeatures {
     }
 
     paginate(){
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
+        const page = this.queryString.page * 1 || 1;
+        const limit = this.queryString.limit * 1 || 100;
         const skip = (page - 1) * limit;
         // page=2&limit=10 , 1-10, page 1, 11-20, page 2, 21-30 page 3
         this.query = this.query.skip(skip).limit(limit);
 
-        if ( req.query.page){
-            const numTours = await Tour.countDocuments();
-            if ( skip >= numTours) throw new Error('This page does not exist');
-        }
         return this;
     }
 }
