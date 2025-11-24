086a578e7825bf40ab0ac2e7c56ca432ef8f4601
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-24 22:02:39 +0100

    create aggregation about tours dates

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -244,11 +244,25 @@ exports.getTourStats = async (req, res) => {
 
 exports.getMonthlyPlan = async (req, res)=> {
     try {
-        const year = req.params.year * 1;
+        const year = req.params.year * 1; // 2021 year
 
         const plan = await Tour.aggregate([
             {
                 $unwind: '$startDates'
+            },
+            {
+                $match: { 
+                    startDates: {
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`)
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates'},
+                    numTourStarts: { $sum: 1}
+                }
             }
         ];)
 
