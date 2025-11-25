7afa38eea2576125f8148f4e63e1290dcbfaec72
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-26 00:34:49 +0100

    issue with server

--- 4-natours/starter/utils/api-features.js
+++ 4-natours/starter/utils/api-features.js
@@ -19,7 +19,7 @@ class APIFeatures {
 
     sort(){
         if (this.queryString.sort){
-            const sortBy = req.query.sort.split(',').join(' ');
+            const sortBy = this.queryString.sort.split(',').join(' ');
             this.query = this.query.sort(sortBy)
         } else {
             this.query = this.query.sort('-createdAt');
@@ -29,7 +29,7 @@ class APIFeatures {
     }
     limitFields(){
         if ( this.queryString.fields){
-            const fields = req.query.fields.split(',').join(' ');
+            const fields = this.queryString.fields.split(',').join(' ');
             this.query = this.query.select(fields);
         }else {
             this.query = this.query.select('-__v')
