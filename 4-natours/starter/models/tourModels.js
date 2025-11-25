b94cebc09e3f6dd16fb082ac9de6976dfea75dec
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-26 00:43:43 +0100

    built-in validators

--- 4-natours/starter/models/tourModels.js
+++ 4-natours/starter/models/tourModels.js
@@ -21,11 +21,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult'
+      }
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
